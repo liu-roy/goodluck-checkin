f5855ddc0b2985b553347eadf41972104feff8ae
liu-roy <liu_roy@outlook.com>
2024-11-23 23:53:16 +0800

    启用定时任务

--- goodluck-checkin-service/src/main/java/com/goodluck/checkin/Application.java
+++ goodluck-checkin-service/src/main/java/com/goodluck/checkin/Application.java
@@ -8,8 +8,10 @@ import org.springframework.cache.annotation.EnableCaching;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
+@EnableScheduling
 //@EnableFeignClients
 //@EnableDiscoveryClient
 //@MapperScan({"com.goodluck.checkin.domain.*.mapper"})
