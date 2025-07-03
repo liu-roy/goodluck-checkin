e4ae912b6ed9e98055aee12e606a5d556d06b740
liu-roy <liu_roy@outlook.com>
2025-07-03 17:12:25 +0800

    Update AbstractCheckinService.java
    补全缺少的类

--- goodluck-checkin-service/src/main/java/com/goodluck/checkin/site/AbstractCheckinService.java
+++ goodluck-checkin-service/src/main/java/com/goodluck/checkin/site/AbstractCheckinService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.File;
 import java.io.IOException;
